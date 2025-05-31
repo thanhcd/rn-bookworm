63500a47e6e4ae794bd621d24293719a5d9d2787
thanhcd <chauducthanh02@gmail.com>
2025-05-31 15:13:43 +0700

    cron.js added 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import job from './lib/cron.js';
 
 import authRoutes from './routes/authRoutes.js';
 import { connectDB } from './lib/db.js';
@@ -9,6 +10,7 @@ import bookRoutes from './routes/bookRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start(); // Bắt đầu cron job
 app.use(express.json()); // cần thiết để đọc JSON body
 
 app.use("/api/auth", authRoutes);
