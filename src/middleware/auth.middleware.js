5f47941c5c3df78f87e44d2e82e0d379e472ef95
thanhcd <chauducthanh02@gmail.com>
2025-06-02 15:36:14 +0700

    change limit1

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,20 +3,21 @@ import User from "../models/User.js";
 
 const protectRoute = async (req, res, next) => {
     try {
-        //get token
-        const token = req.headers("Authoriation").replace("Bearer", "");
-        if (!token) {
+        const authHeader = req.headers['authorization'] || req.headers.Authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(401).json({ message: "No token provided, authorization denied" });
         }
+
+        const token = authHeader.replace("Bearer ", "").trim();
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
+
         const user = await User.findById(decoded.userId).select("-password");
         if (!user) {
             return res.status(401).json({ message: "Invalid token, authorization denied" });
         }
 
-        req.user = user; // Attach user to request object
-        next(); // Call the next middleware or route handler
+        req.user = user;
+        next();
     } catch (error) {
         console.error("Error in protectRoute middleware:", error);
         res.status(500).json({ message: "Internal server error" });
